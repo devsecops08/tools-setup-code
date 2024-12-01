8186b591afa7d0687d2b6d265ca88ba58bddac18
Your Name <you@example.com>
2024-12-01 12:28:07 +0530

    add

--- module-ec2/main.tf
+++ module-ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "sg" {
-  name        = "${var.component_name}-${var.env}-sg"
-  description = "Inbound allow for ${var.component_name}"
+  name        = "${var.tool_name}-sg"
+  description = "Inbound allow for ${var.tool_name}"
 
   
 
@@ -12,8 +12,8 @@ resource "aws_security_group" "sg" {
   }
 
   ingress {
-    from_port        = var.app_port
-    to_port          = var.app_port
+    from_port        = var.sg_port
+    to_port          = var.sg_port
     protocol         = "TCP"
     cidr_blocks      = ["0.0.0.0/0"]  
   }
@@ -25,6 +25,9 @@ resource "aws_security_group" "sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     
   }
+    tags = {
+      name ="${var.tool_name}"
+    }
 }
 
 resource "aws_instance"  "instance" {
@@ -33,32 +36,23 @@ resource "aws_instance"  "instance" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
-    Name = "${var.component_name}-${var.env}"
+    Name = var.tool_name
   }
 }
 
-resource "null_resource"  "ansible-pull" {
 
-  provisioner "remote-exec" {
 
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      password = "DevOps321"
-      host     = aws_instance.instance.private_ip
-    }
-
-    inline = [
-
-      "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github.com/its-amanihub/roboshop-ansible.git roboshop.yml -e env=${var.env} -e app_name=${var.component_name}"
-    ]
-  }
-}
-
-resource "aws_route53_record"  "record" {
+resource "aws_route53_record"  "record-public" {
   zone_id = var.zone_id
-  name    = "${var.component_name}-${var.env}.${var.domain_name}"
+  name    = "${var.tool_name}.${var.domain_name}"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.instance.public_ip]
+}
+
+resource "aws_route53_record"  "record-internal" {
+  zone_id = var.zone_id
+  name    = "${var.tool_name}-internal.${var.domain_name}"
   type    = "A"
   ttl     = "30"
   records = [aws_instance.instance.private_ip]
--- variables.tf
+++ variables.tf
@@ -4,6 +4,14 @@ variable "tools"  {
     vault =  {
        port= 8200
        volume_size= 20 
+       instance_type="t3.small"
     }
 }
+}
+
+variable "zone_id" {
+  default = "Z04036722QT9R780VLSOQ"
+}
+variable "domain_name" {
+  default = "adevsecops08.online"
 }
