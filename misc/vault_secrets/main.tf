af6d7a9b350d7e7dc882f64a598fe0b4738b64f6
Your Name <you@example.com>
2024-12-01 17:31:22 +0530

    add

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -21,20 +21,88 @@ resource "vault_mount" "roboshop-dev" {
   description = "RoboShop Dev Secrets"
 }
 
-resource "vault_generic_secret" "roboshop-dev" {
-  path = "${vault_mount.roboshop-dev.path}/frontend"
+
+resource "vault_generic_secret" "catalogue" {
+  path = "${vault_mount.roboshop-dev.path}/catalogue"
 
   data_json = <<EOT
 {
-  "catalogue_url":   "http://catalogue-dev.adevsecops08.online:8080/",
-  "cart_url":   "http://cart-dev.adevsecops08.online:8080/",
-  "user_url":   "http://user-dev.adevsecops08.online:8080/",
-  "shipping_url":   "http://shipping-dev.adevsecops08.online:8080/",
-  "payment_url":   "http://payment-dev.adevsecops08.online:8080/"
-  
-  
-  
+ MONGO: "true"
+"MONGO_URL" : "mongodb://mongodb-dev.adevsecops08.online:27017/catalogue"
+   
 }
 EOT
 }
-  
+
+resource "vault_generic_secret" "user" {
+  path = "${vault_mount.roboshop-dev.path}/user"
+
+  data_json = <<EOT
+{
+  "MONGO": "true",
+  "MONGO_URL" : "mongodb://mongodb-dev.adevsecops08.online:27017/users",
+  "REDIS_URL" : "redis://redis-dev.adevsecops08.online:6379"
+}
+EOT
+}
+
+resource "vault_generic_secret" "cart" {
+  path = "${vault_mount.roboshop-dev.path}/cart"
+
+  data_json = <<EOT
+{
+  "REDIS_HOST": "redis-dev.adevsecops08.online",
+  "CATALOGUE_HOST" : "catalogue-dev.adevsecops08.online",
+  "CATALOGUE_PORT" : "8080"
+}
+EOT
+}
+
+resource "vault_generic_secret" "shipping" {
+  path = "${vault_mount.roboshop-dev.path}/shipping"
+
+  data_json = <<EOT
+{
+  "REDIS_HOST": "redis-dev.adevsecops08.online",
+  "CATALOGUE_HOST" : "catalogue-dev.adevsecops08.online",
+  "CATALOGUE_PORT" : "8080"
+}
+EOT
+}
+
+resource "vault_generic_secret" "payment" {
+  path = "${vault_mount.roboshop-dev.path}/payment"
+
+  data_json = <<EOT
+{
+  "REDIS_HOST": "redis-dev.adevsecops08.online",
+  "CATALOGUE_HOST" : "catalogue-dev.adevsecops08.online",
+  "CATALOGUE_PORT" : "8080"
+}
+EOT
+}
+
+resource "vault_generic_secret" "mysql" {
+  path = "${vault_mount.roboshop-dev.path}/mysql"
+
+  data_json = <<EOT
+{
+  "REDIS_HOST": "redis-dev.adevsecops08.online",
+  "CATALOGUE_HOST" : "catalogue-dev.adevsecops08.online",
+  "CATALOGUE_PORT" : "8080"
+}
+EOT
+}
+
+resource "vault_generic_secret" "rabbitmq" {
+  path = "${vault_mount.roboshop-dev.path}/rabbitmq"
+
+  data_json = <<EOT
+{
+  "REDIS_HOST": "redis-dev.adevsecops08.online",
+  "CATALOGUE_HOST" : "catalogue-dev.adevsecops08.online",
+  "CATALOGUE_PORT" : "8080"
+}
+EOT
+}
+
