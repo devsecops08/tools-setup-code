c5bf854c0423ba41936ec1177547c7f2401fd2ec
Your Name <you@example.com>
2024-12-31 15:10:18 +0530

    spot price

--- variables.tf
+++ variables.tf
@@ -6,6 +6,12 @@ variable "tools"  {
        volume_size= 20 
        instance_type="t3.small"
     }
+    github-runner ={
+      port= 80
+      volume_size= 20
+      instance_type="t3.small"
+    }
+
 }
 }
 
